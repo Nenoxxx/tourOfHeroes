89b96842d6df48cc414b419cce9d69bfe30de067
Edon Malushaj <edon_malushaj@sluz.ch>
2024-01-08 10:48:30 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Hero } from '../declarations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'testProject';
+  selectedHero?: Hero;
+
+  heroes: Hero[] = [
+    { id: 12, name: 'Dr. Nice', superpower: 'Flying' },
+    { id: 13, name: 'Bombasto' },
+  ];
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
